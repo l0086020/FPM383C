967bceeb8456becf2caae19cd1d68c86b2048944
世界不大我姓张 <mr_zh-ang@qq.com>
2022-08-13 22:20:14 +0800

    updata STM32 Code

--- 代码/STM32/FPM383C.c
+++ 代码/STM32/FPM383C.c
@@ -1,20 +1,16 @@
 /********************************************************************************************************************
+
 v1.0因为使用的是While循环等待接收的数据，可能会因为数据接收出错或其他原因，导致程序卡死，或者将程序更改为while(--Timeout),用延时来等待接收数据。
 v1.1使用IWDG独立看门狗2秒定时进行复位，这种方法可有效避免程序卡死。
 
-此示例只进行了简单的验证指纹，注册函数都没有写，可以借鉴我的ESP8266写的代码，GitHub：https://github.com/l0086020/FPM383C.git
-
 中断引脚接在PC13上
-.h文件里没有包含其他东西，就是函数原型，所以就不上传了。
 
 在main函数里调用一下FPM383C_Init()就可以了。
 
-可能要加上USART1的初始化，因为有调试信息输出，也可以把void ACK(uint8_t ack);里面的所有的Printf全删了都可以。
-
-需要包含delay文件、stm32芯片头文件、NVIC中断向量等等，就是缺啥自己加啥，指纹模块的功能都是可用的。
 ********************************************************************************************************************/
 
-#include "main.h"
+#include "stdio.h"
+#include "stm32f10x.h"
 
 uint8_t Receive_STA = 0;
 uint16_t PageID,MatchScore;
@@ -28,6 +24,15 @@ uint8_t PS_GreenLEDBuffer[16] = {0xEF,0x01,0xFF,0xFF,0xFF,0xFF,0x01,0x00,0x07,0x
 //模块睡眠协议
 uint8_t PS_SleepBuffer[12] = {0xEF,0x01,0xFF,0xFF,0xFF,0xFF,0x01,0x00,0x03,0x33,0x00,0x37};
 
+//清空指纹协议
+uint8_t PS_EmptyBuffer[12] = {0xEF,0x01,0xFF,0xFF,0xFF,0xFF,0x01,0x00,0x03,0x0D,0x00,0x11};
+
+//取消命令协议
+uint8_t PS_CancelBuffer[12] = {0xEF,0x01,0xFF,0xFF,0xFF,0xFF,0x01,0x00,0x03,0x30,0x00,0x34};
+
+//自动注册指纹协议
+uint8_t PS_AutoEnrollBuffer[17] = {0xEF,0x01,0xFF,0xFF,0xFF,0xFF,0x01,0x00,0x08,0x31,'\0','\0',0x04,0x00,0x16,'\0','\0'}; //PageID: bit 10:11，SUM: bit 15:16
+
 //模块搜索用获取图像协议
 uint8_t PS_GetImageBuffer[12] = {0xEF,0x01,0xFF,0xFF,0xFF,0xFF,0x01,0x00,0x03,0x01,0x00,0x05};
 
@@ -37,6 +42,41 @@ uint8_t PS_GetChar2Buffer[13] = {0xEF,0x01,0xFF,0xFF,0xFF,0xFF,0x01,0x00,0x04,0x
 
 //模块搜索指纹功能协议
 uint8_t PS_SearchMBBuffer[17] = {0xEF,0x01,0xFF,0xFF,0xFF,0xFF,0x01,0x00,0x08,0x04,0x01,0x00,0x00,0xFF,0xFF,0x02,0x0C};
+
+	
+/**
+	* @brief	微秒级延时函数
+	* @param 	us：需要延时的时间，单位微秒
+	* @return None
+	*/
+void delay_us(uint32_t us)
+{
+    uint32_t i;
+    SysTick_Config(SystemCoreClock/1000000);	//配置SysTick为10us
+    for(i=0; i<us; i++)
+    {
+        while(!((SysTick->CTRL) & (1<<16)));	//等待计数器到0，CTRL寄存器的位16会置1，当置1时，读取该位会清0
+    }
+    SysTick->CTRL &= ~SysTick_CTRL_ENABLE_Msk;	//关闭SysTick定时器
+}
+
+
+/**
+	* @brief	毫秒级延时函数
+	* @param 	ms：需要延时的时间，单位毫秒
+	* @return None
+	*/
+void delay_ms(uint32_t ms)
+{
+    uint32_t i;
+    SysTick_Config(72000);	//配置SysTick为1ms
+    for(i=0; i<ms; i++)
+    {
+        while(!((SysTick->CTRL) & (1<<16)));	//等待计数器到0，CTRL寄存器的位16会置1，当置1时，读取该位会清0
+    }
+    SysTick->CTRL &= ~SysTick_CTRL_ENABLE_Msk;	//关闭SysTick定时器
+}
+
 
 /**
 	* @brief	USART2串口发送函数
@@ -136,6 +176,22 @@ uint8_t FPM383C_SearchMB(uint32_t Timeout)
 
 
 /**
+	* @brief	清空指纹库
+	* @param 	Timeout：接收数据的超时时间
+	* @return 确认码
+	*/
+uint8_t FPM383C_Empty(uint32_t Timeout)
+{
+	FPM383C_SendData(12,PS_EmptyBuffer);
+	while(Receive_STA<12 && (--Timeout))
+	{
+		delay_ms(1);
+	}
+	return FPM383C_ReceiveBuffer[9];
+}
+
+
+/**
 	* @brief	发送控制灯光指令
 	* @param 	PS_ControlLEDBuffer[]：不同颜色的协议数据
 	* @return None
@@ -172,15 +228,34 @@ uint8_t FPM383C_Identify()
 }
 
 /**
-	* @brief	清空指纹库，还没有写，可以参考一下我的ESP8266的代码，GitHub链接：https://github.com/l0086020/FPM383C.git
-	* @param 	None
-	* @return 确认码
+	* @brief	注册指纹函数
+	* @param 	PageID：输入需要注册的指纹ID号，取值范围0—59
+	* @param	Timeout：设置注册指纹超时时间，因为需要按压四次手指，建议大于10000（即10s）
+	* @return 确认码or无效码
 	*/
-uint8_t FPM383C_Empty()
+uint8_t FPM383C_Enroll(uint16_t PageID,uint16_t Timeout)
 {
-	//FPM383C_SendData(12,FPM383C_EmptyBuffer);
-	while(Receive_STA<12);
-	return FPM383C_ReceiveBuffer[9];
+	PS_AutoEnrollBuffer[10] = (PageID>>8);
+  PS_AutoEnrollBuffer[11] = (PageID);
+  PS_AutoEnrollBuffer[15] = (0x54+PS_AutoEnrollBuffer[10]+PS_AutoEnrollBuffer[11])>>8;
+  PS_AutoEnrollBuffer[16] = (0x54+PS_AutoEnrollBuffer[10]+PS_AutoEnrollBuffer[11]);
+  FPM383C_SendData(17,PS_AutoEnrollBuffer);
+  while(Receive_STA<12 && (--Timeout))
+	{
+		delay_ms(1);
+	}
+	if(FPM383C_ReceiveBuffer[9] == 0x00)
+	{
+		FPM383C_ControlLED(PS_GreenLEDBuffer);
+		return FPM383C_ReceiveBuffer[9];
+	}
+	else if(Timeout == 0)
+	{
+		FPM383C_SendData(12,PS_CancelBuffer);
+		delay_ms(50);
+	}
+	FPM383C_ControlLED(PS_RedLEDBuffer);
+	return 0xFF;
 }
 
 
@@ -254,12 +329,71 @@ void ACK(uint8_t ack)
 }
 
 
+
+/**
+	* @brief	中断向量函数，里面配置所需要的中断功能
+	* @param 	None
+	* @return None
+	*/
+void NVIC_Config(void)
+{
+	NVIC_InitTypeDef NVIC_InitStructure;
+	
+	NVIC_SetPriorityGrouping(NVIC_PriorityGroup_2);
+
+	NVIC_InitStructure.NVIC_IRQChannel = EXTI15_10_IRQn;
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 2;
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1;
+	NVIC_Init(&NVIC_InitStructure);
+	
+	NVIC_InitStructure.NVIC_IRQChannel = USART2_IRQn;
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 1;
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1;
+	NVIC_Init(&NVIC_InitStructure);
+}
+
+/**
+	* @brief	USART1串口初始化函数（可要可不要，考虑到可能需要Debug，所以先初始化一遍）
+	* @param 	None
+	* @return None
+	*/
+void USART_1_Config(void)
+{
+	GPIO_InitTypeDef GPIO_InitStructure;
+	USART_InitTypeDef USART_InitStructure;
+	
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA,ENABLE);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1,ENABLE);
+	
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOA,&GPIO_InitStructure);
+	
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;
+	GPIO_Init(GPIOA,&GPIO_InitStructure);
+	
+	USART_InitStructure.USART_BaudRate = 9600;
+	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+	USART_InitStructure.USART_Mode = USART_Mode_Rx|USART_Mode_Tx;
+	USART_InitStructure.USART_Parity = USART_Parity_No;
+	USART_InitStructure.USART_StopBits = USART_StopBits_1;
+	USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+	USART_Init(USART1,&USART_InitStructure);
+	
+	USART_Cmd(USART1,ENABLE);
+}
+
+
 /**
 	* @brief	USART2 串口初始化函数
 	* @param 	None
 	* @return None
 	*/
-void USART_2_Config()
+void USART_2_Config(void)
 {
 	GPIO_InitTypeDef GPIO_InitStructure;
 	USART_InitTypeDef USART_InitStructure;
@@ -267,7 +401,7 @@ void USART_2_Config()
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA,ENABLE);
 	RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2,ENABLE);
 	
-	NVIC_Config();	//中断向量，自己写
+	NVIC_Config();
 	
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
 	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2;
@@ -331,10 +465,22 @@ void FPM383C_GPIO_Interrupt_Config()
 	*/
 void FPM383C_Init(void)
 {
+	USART_1_Config();
 	USART_2_Config();
 	FPM383C_GPIO_Interrupt_Config();
 }
 
+/**
+	* @brief	printf重映射函数，重映射后可以直接使用printf函数，并直接打印到USART1串口上
+	* @param 	unknow
+	* @return unknow
+	*/
+int fputc(int ch,FILE *f)
+{
+	USART_SendData(USART1,(uint8_t)ch);
+	while(!USART_GetFlagStatus(USART1,USART_FLAG_TXE));
+	return ch;
+}
 
 /**
 	* @brief	外部中断函数
@@ -350,8 +496,8 @@ void EXTI15_10_IRQHandler(void)
 		GPIO_SetBits(GPIOC,GPIO_Pin_0);
 		delay_ms(50);
 		GPIO_ResetBits(GPIOC,GPIO_Pin_0);
-		ACK(FPM383C_Identify());		//验证指纹模式
-		//ACK(FPM383C_Enroll());		//注册指纹模式
+		//ACK(FPM383C_Identify());		//验证指纹模式
+		ACK(FPM383C_Enroll(0,10000));		//注册指纹模式
 	}
 	FPM383C_Sleep(1);
 }
